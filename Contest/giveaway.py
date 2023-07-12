445b138804942e16723b91a17bec5628dcdd1388
Josh Madakor <39254979+joshmadakor1@users.noreply.github.com>
2023-07-12 00:31:56 -0700

    Update giveaway.py

--- Contest/giveaway.py
+++ Contest/giveaway.py
@@ -7,20 +7,22 @@ import csv
 YOUTUBE_API_KEY = 'xxxxxxxxxxxxxxxxxxxxxx'
 
 '''
-0:04 NIST
-4:58 PCI
-12:41 CISO
-15:53 800-53
-17:46 RMF
+00:10 - BE
+02:05 - SMART
+05:02 - WITH
+10:46 - YOUR
+12:32 - COINS
 '''
 # Replace with the words viewers need to discover in the video
-SECRET_WORDS = ['NIST', 'PCI', 'CISO', '800-53', 'RMF']
+SECRET_WORDS = ['BE', 'SMART', 'WITH', 'YOUR', 'COINS']
 
 # Replace with the video ID of your target video: https://www.youtube.com/watch?v=LweW1RypjD4
 VIDEO_ID = 'LweW1RypjD4'
 
 # These are the headers for the CSV files containing the potential winners
-CSV_HEADER = ['author_display_name', 'author_channel_url', 'comment', 'comment_link', 'comment_id', 'comment_date', 'video_id']
+CSV_HEADER = ['author_display_name', 'author_channel_url', 'comment',
+              'comment_link', 'comment_id', 'comment_date', 'video_id']
+
 
 def collect_comments_from_youtube_video(api_key, video_id):
 
@@ -52,30 +54,31 @@ def collect_comments_from_youtube_video(api_key, video_id):
     # print all the comment details
     for comment in comments:
         all_comments[comment['id']] = {
-                                       "author_display_name": comment['snippet']['topLevelComment']['snippet']['authorDisplayName'],
-                                       "author_channel_url": comment['snippet']['topLevelComment']['snippet']['authorChannelUrl'],
-                                       "comment": comment['snippet']['topLevelComment']['snippet']['textDisplay'],
-                                       "comment_id": comment['id'],
-                                       "comment_date": comment['snippet']['topLevelComment']['snippet']['publishedAt'],
-                                       "video_id": video_id
-                                       }
+            "author_display_name": comment['snippet']['topLevelComment']['snippet']['authorDisplayName'],
+            "author_channel_url": comment['snippet']['topLevelComment']['snippet']['authorChannelUrl'],
+            "comment": comment['snippet']['topLevelComment']['snippet']['textDisplay'],
+            "comment_id": comment['id'],
+            "comment_date": comment['snippet']['topLevelComment']['snippet']['publishedAt'],
+            "video_id": video_id
+        }
         # If the top-level comment has replies in it
         if 'replies' in comment:
             for reply in comment['replies']:
                 all_comments[reply['id']] = {
-                                            "author_display_name": reply['snippet']['authorDisplayName'],
-                                            "author_channel_url": reply['snippet']['authorChannelUrl'], 
-                                            "comment": reply['snippet']['textDisplay'],
-                                            "comment_id": reply['id'],
-                                            "comment_date": comment['snippet']['topLevelComment']['snippet']['publishedAt'],
-                                            "video_id": video_id}
+                    "author_display_name": reply['snippet']['authorDisplayName'],
+                    "author_channel_url": reply['snippet']['authorChannelUrl'],
+                    "comment": reply['snippet']['textDisplay'],
+                    "comment_id": reply['id'],
+                    "comment_date": comment['snippet']['topLevelComment']['snippet']['publishedAt'],
+                    "video_id": video_id}
                 print(
                     f"Reply by {reply['snippet']['authorDisplayName']}: {reply['snippet']['textDisplay']}")
 
     return all_comments
 
+
 def collect_comments_containing_secret_words(secret_words, all_comments):
-    
+
     random.shuffle(secret_words)
 
     comments_with_secret_words = list()
@@ -85,8 +88,9 @@ def collect_comments_containing_secret_words(secret_words, all_comments):
             if secret_word.lower() in value['comment'].lower():
                 if value not in comments_with_secret_words:
                     comments_with_secret_words.append(value)
-    
+
     return comments_with_secret_words
+
 
 def delete_file_if_exists(filename):
     if os.path.isfile(filename):
@@ -95,6 +99,7 @@ def delete_file_if_exists(filename):
     else:
         return False
 
+
 def write_csv_header(filename, header):
 
     # Clean up old CSV file if it exists
@@ -102,20 +107,22 @@ def write_csv_header(filename, header):
 
     with open(filename, 'a', newline='') as file:
         writer = csv.writer(file, delimiter='\t')
-        
+
         # Append the data to the CSV file
         writer.writerow(header)
 
-def remove_linebreaks(s):   
+
+def remove_linebreaks(s):
     # remove line breaks
     s = s.replace('\n', ' ').replace('\r', ' ')
-    
+
     return s
+
 
 def write_line_to_winner_file(filename, comment):
     with open(filename, 'a', newline='') as file:
         writer = csv.writer(file, delimiter='\t')
-        
+
         # Replace all linebreaks with spaces.
         comment['comment'] = remove_linebreaks(comment['comment'])
 
@@ -128,7 +135,8 @@ def write_line_to_winner_file(filename, comment):
             comment['comment_id'],
             comment['comment_date'],
             comment['video_id']
-            ])
+        ])
+
 
 def record_winners_of_each_secret_word_to_csv(secret_words, comments_with_secret_words):
     for secret_word in secret_words:
@@ -143,18 +151,22 @@ def record_winners_of_each_secret_word_to_csv(secret_words, comments_with_secret
             if secret_word.lower() in comment['comment'].lower():
                 write_line_to_winner_file(filename=filename, comment=comment)
 
+
 # Collect all the comments from target video and store in list
-all_comments = collect_comments_from_youtube_video(api_key=YOUTUBE_API_KEY, video_id=VIDEO_ID)
+all_comments = collect_comments_from_youtube_video(
+    api_key=YOUTUBE_API_KEY, video_id=VIDEO_ID)
 print(f"Total comments in video: {len(all_comments)}")
 
 # From all comments, select only the comments that contain secret words and store them in a list
-comments_with_secret_words = collect_comments_containing_secret_words(secret_words=SECRET_WORDS, all_comments=all_comments)
+comments_with_secret_words = collect_comments_containing_secret_words(
+    secret_words=SECRET_WORDS, all_comments=all_comments)
 print(f"Comments containing secret words: {len(comments_with_secret_words)}")
 
 # Shuffle the comments containing secret words (This is where the winners for each secret word will be determined)
 random.shuffle(comments_with_secret_words)
 
 # Write the comments containing secret words out to their own respective CSV Files
-record_winners_of_each_secret_word_to_csv(secret_words=SECRET_WORDS, comments_with_secret_words=comments_with_secret_words)
+record_winners_of_each_secret_word_to_csv(
+    secret_words=SECRET_WORDS, comments_with_secret_words=comments_with_secret_words)
 
 print("fin")
